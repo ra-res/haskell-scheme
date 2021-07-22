33719fa18b84ed6717adf5d3855ff0a63b6132fd
ra-res <ra-res@users.noreply.github.com>
2021-07-22 21:54:18 +0100

    Add LispVal datastructure to parser

--- Main.hs
+++ Main.hs
@@ -5,8 +5,37 @@ import Text.ParserCombinators.Parsec hiding (spaces)
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~" 
 
+spaces :: Parser ()
+spaces = skipMany1 space
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do 
+    char '"'
+    x <- many (noneOf "\"")
+    char '"'
+    return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+    first <- letter <|> symbol
+    rest <- many (letter <|> digit <|> symbol)
+    let atom = first:rest
+    return $ case atom of 
+        "#t" -> Bool True
+        "#f" -> Bool False
+        _ -> Atom atom
+
+
+
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of 
+readExpr input = case parse (spaces >> symbol) "lisp" input of 
     Left err -> "No match : " ++ show err
     Right val -> "Found value"
 
