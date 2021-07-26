c5620f25711dc8e227d528dd4dc8d0afcd4145e3
ra-res <ra-res@users.noreply.github.com>
2021-07-26 21:14:41 +0100

    Add list parser

--- Main.hs
+++ Main.hs
@@ -48,16 +48,38 @@ parseFloat = do
     let atom = (x ++ "." ++ y)
     return $ Float $ read atom
 
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of 
+    Left err -> "No match : " ++ show err
+    Right val -> "Found value"
+
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+  head <- endBy parseExpr spaces
+  tail <- char '.' >> spaces >> parseExpr
+  return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+  char '\''
+  x <- parseExpr 
+  return $ List [Atom "quote", x]
+
 parseExpr :: Parser LispVal
 parseExpr = parseAtom 
         <|> parseString
         <|> parseFloat
         <|> parseNumber
+        <|> parseQuoted
+        <|> do 
+                char '('
+                x <- (try parseList) <|> parseDottedList
+                char ')'
+                return x
 
-readExpr :: String -> String
-readExpr input = case parse parseExpr "lisp" input of 
-    Left err -> "No match : " ++ show err
-    Right val -> "Found value"
 
 main :: IO ()
 main = do
